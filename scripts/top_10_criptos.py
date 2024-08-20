a1154b599c9b733f27994cc3612ab136d570ef5a
Gustavo <gustavoadorno3@gmail.com>
2024-08-20 14:35:53 -0300

    Extrair e salvar Top 10 criptos bruto

--- scripts/top_10_criptos.py
+++ scripts/top_10_criptos.py
@@ -2,6 +2,7 @@ import os
 import requests
 import pandas as pd
 
+# Função para extrair os dados da API
 def top_10_criptos():
     url = 'https://api.coingecko.com/api/v3/coins/markets'
     params = {
@@ -12,7 +13,6 @@ def top_10_criptos():
     }
     
     response = requests.get(url, params=params)
-    
     if response.status_code == 200:
         data = response.json()
         return pd.DataFrame(data)
@@ -20,25 +20,16 @@ def top_10_criptos():
         print(f"Erro ao buscar dados da API: {response.status_code}")
         return None
 
+# Função para salvar os dados brutos 
 def dados_brutos_top10(df, filename='top_10_criptos.csv'):
-    # Diretório onde os dados brutos serão salvos
     raw_data_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data', 'raw')
-    
-    # Cria o diretório se não existir
-    os.makedirs(raw_data_path, exist_ok=True)
-    
-    # Caminho completo do arquivo
     file_path = os.path.join(raw_data_path, filename)
-    
-    # Salva o DF como CSV
     df.to_csv(file_path, index=False)
     print(f"Dados brutos salvos em: {file_path}")
-
 if __name__ == "__main__":
-    # Buscar os dados das 10 maiores criptomoedas
+
+# Extraindo e salvando os dados brutos    
     df_criptos = top_10_criptos()
-    
     if df_criptos is not None:
-        # Salvar os dados brutos na pasta raw
         dados_brutos_top10(df_criptos)
-        print("Dados brutos coletados e salvos com sucesso.")
+        print("Dados brutos extraidos e salvos com sucesso.")
